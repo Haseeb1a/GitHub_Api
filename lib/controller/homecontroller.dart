9f27f7376e93e8e9aa9876ab97d7c293e650be8f
Haseeb1a <mh3824555@gmail.com>
2023-11-02 16:57:04 +0530

    poviderupdates2

--- lib/controller/homecontroller.dart
+++ lib/controller/homecontroller.dart
@@ -1,15 +1,18 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:github/controller/profilepage.dart';
+import 'package:provider/provider.dart';
 
 import '../model/githubmodel.dart';
 import '../services/gitservices.dart';
 
 class Homecontroller extends ChangeNotifier {
-
   String searched = "hudaifc";
-    final TextEditingController searchcontroller = TextEditingController();
-     clearsearch() {
-      searchcontroller.clear();
-    }
-    
-  
+  final TextEditingController searchcontroller = TextEditingController();
+  clearsearch() {
+    searchcontroller.clear();
+  }
+
+
 }
